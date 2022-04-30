d486f02a1b690c67ab26d218c379702c76740ca8
Helio Albano de Oliveira <helioalb@gmail.com>
2022-04-30 12:26:26 -0300

    JPA entities
    Reference: https://www.baeldung.com/jpa-entities

---
+++ src/main/java/me/helioalbano/jpaandhibernate/Student.java
@@ -0,0 +1,37 @@
+package me.helioalbano.jpaandhibernate;
+
+import java.util.Date;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "students", schema = "school")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false, unique = false)
+    private String name;
+
+    @Transient
+    private Integer age;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+}
+
