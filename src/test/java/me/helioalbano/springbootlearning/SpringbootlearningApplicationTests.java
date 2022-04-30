034cb7a5a43bf62cdb0ec6eac1e22d4169cd79d4
Helio Albano de Oliveira <helioalb@gmail.com>
2022-04-30 11:09:53 -0300

    Start project :rocket:

---
+++ src/test/java/me/helioalbano/springbootlearning/SpringbootlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package me.helioalbano.springbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
